5b750d832af81b4fbd3b8dbe6f25c412893c62b7
JuneAlSi <myinboxdev@gmail.com>
2018-05-16 16:58:51 +0700

    complete

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# facebook-login-in-react
+Creating a simple component React like facebook's login
---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, {
+  Component
+} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Facebook from './components/facebook'
+
+class App extends Component {
+  render() {
+    return ( <
+      div className = "App" >
+      <
+      header className = "App-header" >
+      <
+      img src = {
+        logo
+      }
+      className = "App-logo"
+      alt = "logo" / >
+      <
+      h1 className = "App-title" > Facebook Login Example < /h1> < /
+      header > <
+      p className = "App-intro" >
+      To get started, please login. <
+      /p>  <
+      Facebook / >
+      <
+      /
+      div >
+    );
+  }
+}
+
+export default App;
